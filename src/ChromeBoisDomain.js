978a5c8750b55cd1dcc749b9fe4fc3b3aab16acc
certilremy <certilremy@gmail.com>
2019-02-26 12:59:32 -0500

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,30 +3,27 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
+
   handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
+    const x = event.clientX;
+    const y = event.clientY;
+    drawChromeBoiAtCoords(x, y);
   }
-  
-  /* TODO: Create an event handler which, when fired, invokes the provided
-   * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
-   */
-   
-  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
-  /* is pressed. When a key is pressed, an event handler should invoke the the
-  /* provided `resize` function with a single argument of either '+' or '-'
-  /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
-   */
-  
+
+  keyPressed = (event) => {
+    if(event.key == 'a'){
+      resize('+'); //was having error b/c not in quotes!
+    } else if (event.key == 's') {
+      resize('-');
+    }
+  }
+
+
   render() {
     return (
-      <canvas 
+      <canvas
+        onClick={toggleCycling()}
+        onKeyPress={this.keyPressed} //before had parentheses which was causing error
         onMouseMove={this.handleMouseMove}
         width='900'
         height='600'
