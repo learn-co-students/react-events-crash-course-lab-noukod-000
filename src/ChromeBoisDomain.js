bc829bf43236571ad7636e5a49df8955aeba960d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-01 21:03:40 +0000

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -13,21 +13,21 @@ export default class ChromeBoisDomain extends Component {
      drawChromeBoiAtCoords(event.clientX, event.clientY);
   }
 
-  /* TODO: Create an event handler which, when fired, invokes the provided
-   * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
-   */
    handleMouseClick = (event) => {
+     /* TODO: Create an event handler which, when fired, invokes the provided
+      * `toggleCycling` function with no arguments. Don't forget the click event
+      * listener that should fire it!
+      */
       toggleCycling();
    }
 
-  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
-  /* is pressed. When a key is pressed, an event handler should invoke the the
-  /* provided `resize` function with a single argument of either '+' or '-'
-  /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-'
-   */
    handleKeyUp = (event) => {
+     /* TODO: Add an event listener to the `<canvas>` element to capture when a key
+     /* is pressed. When a key is pressed, an event handler should invoke the the
+     /* provided `resize` function with a single argument of either '+' or '-'
+     /* if the key pressed was 'a', then it should call `resize` with '+'
+     /* if the key pressed was 's', then it should call `resize` with '-'
+      */
      const type = event.key === 'a' ? '+' : (event.key === 's' ? '-' : '');
       resize(type);
    }
